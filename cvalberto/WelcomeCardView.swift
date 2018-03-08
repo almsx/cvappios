1a5cfab8ed64483e192e1c6c2458636bfe698b41
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2018-03-08 16:09:35 -0600

    WelcomeCard

--- cvalberto/WelcomeCardView.swift
+++ cvalberto/WelcomeCardView.swift
@@ -1,9 +1,83 @@
 //
 //  WelcomeCardView.swift
-//  cvalberto
+//  mystory
 //
-//  Created by Alberto Luebbert M. on 08/03/18.
+//  Created by Alberto Luebbert M. on 07/03/18.
 //  Copyright © 2018 Alberto Luebbert M. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Koloda
+import SnapKit
+
+class WelcomeCardView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        addSubviews()
+        setupLayout()
+        clipsToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.size.width / 32
+    }
+    
+    // MARK: Subviews
+    
+    let imageView: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let contentLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.contentMode = .scaleAspectFit
+        return label
+    }()
+    
+    func addSubviews() {
+        addSubview(imageView)
+        addSubview(titleLabel)
+        addSubview(contentLabel)
+    }
+    
+    // MARK: Layout
+    
+    func setupLayout() {
+        imageView.snp.makeConstraints {
+            $0.top.left.right.equalTo(0)
+            $0.height.equalTo(snp.height).multipliedBy(0.85)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(5)
+            $0.left.right.equalTo(0)
+        }
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.left.right.equalTo(0)
+            $0.bottom.equalTo(0)
+        }
+        
+    }
+    
+}
+
