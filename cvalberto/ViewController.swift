c5cd24b3431c99ac0df6fc949d46bb190955e49a
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2018-03-08 15:36:33 -0600

    Initial Commit

---
+++ cvalberto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cvalberto
+//
+//  Created by Alberto Luebbert M. on 08/03/18.
+//  Copyright © 2018 Alberto Luebbert M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
